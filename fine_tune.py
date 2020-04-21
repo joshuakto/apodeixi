4f059abcbd79d0d73a6f78da278ec70889636e69
joshuakto <joshuakto@gmail.com>
2020-04-21 13:32:59 +0800

    updated fine_tune.py argparse options

--- fine_tune.py
+++ fine_tune.py
@@ -11,21 +11,6 @@ from fairseq.data.data_utils import collate_tokens
 import os
 import numpy as np
 
-def push_np_array(l, n):
-	l = np.roll(l, 1)
-	l[0] = n
-	return l
-
-def evaluation(losses, accuracies, steps):
-	running_loss = np.average(losses)
-	running_accu = np.sum(accuracies)	
-	return running_loss, running_accu
-
-def gen_report(batchnumber, running_loss, accuracy, pred, glabel):
-	print('Batch{}| running loss:{}| accu:{}| prediction:{}| ground truth:{}'.format(
-		batchnumber, running_loss, accuracy, pred, glabel)
-		)
-	return
 
 def preprocess_jsonl(datadir):
 	df = pd.read_json(datadir,lines=True)
@@ -49,14 +34,42 @@ def load_batch(df, Number_of_Batches, batch_number, batchsize):
 	sen_pairs = [[s1,s2] for s1, s2 in zip(sen1, sen2)]
 	return batch_i, batch_z, sen_pairs
 
+def extract_labels(df, batch_i, batch_z, label_map):
+	labels = df.loc[batch_i:batch_z, 'gold_label'].values
+	dig_labels = [label_map[l] for l in labels]
+	dig_labels = torch.tensor(dig_labels, dtype = torch.long)
+	return dig_labels
+
+def push_np_array(l, n):
+	l = np.roll(l, 1)
+	l[0] = n
+	return l
+
+def evaluation(losses, accuracies, steps):
+	running_loss = np.average(losses)
+	running_accu = np.sum(accuracies)	
+	return running_loss, running_accu
+
+def gen_report(batch_number, running_loss, running_accu, pred, glabel, writer):
+	print('Batch{}| running loss:{}| accu:{}| prediction:{}| ground truth:{}'.format(
+		batch_number, running_loss, running_accu, pred, glabel)
+		)
+	writer.add_scalar('running_loss', running_loss, batch_number)
+	writer.add_scalar('accuracy', running_accu, batch_number)
+	return
+
 if __name__ == '__main__':
 	# data path setting
 	parser = argparse.ArgumentParser()
 	test_data_path = '/home/bellman/nlp/roberta/data/MultiNLI_StressTest_Evaluation.jsonl'
 	parser.add_argument('--datadir', default=test_data_path)
 	parser.add_argument('--batchsize', default=1)
-	parser.add_argument('--evalsize', default=100)
-	parser.add_argument('--nograd', action='store_true')
+	parser.add_argument('--evalsize', default=100,
+		help='size of running loss/accu np array (to average evaluation metrics)')
+	parser.add_argument('--summarycomment', default='', 
+		help='add string to append to tensorboardX summary writer autonamed file')
+	parser.add_argument('--nograd', action='store_true', 
+		help='enable to disable training e.g. to evaluate model')
 	args = parser.parse_args()
 	df = preprocess_jsonl(args.datadir)
 	# model settings and training settings(loss function and optimizer)
@@ -67,15 +80,15 @@ if __name__ == '__main__':
 	else:
 		roberta.train()
 	roberta.cuda()
+	########### CHANGE optim hyperparams (optimizer/loss func) here ###########
 	criterion = nn.CrossEntropyLoss()
-	########### CHANGE optim hyperparam here ############
 	if not(nograd):
 		optimizer = optim.Adam(roberta.parameters(), lr=1e-8)
 	# initialize array to store evaluations
 	losses = np.zeros(args.evalsize)
 	accuracies = np.zeros(args.evalsize)
 	# log data to tensorboard
-	writer = SummaryWriter(comment='Adam_low_lr')
+	writer = SummaryWriter(comment=args.summarycomment)
 
 	Number_of_Batches = len(df)//args.batchsize+1
 	s = time.time()
@@ -95,9 +108,7 @@ if __name__ == '__main__':
 		# Update model using loss function
 		with torch.set_grad_enabled(not(nograd)):
 			try:
-				labels = df.loc[batch_i:batch_z, 'gold_label'].values
-				dig_labels = [label_map[l] for l in labels]
-				dig_labels = torch.tensor(dig_labels, dtype = torch.long)
+				dig_labels = extract_labels(df, batch_i, batch_z, label_map)
 				dig_labels = dig_labels.to(device='cuda')
 				logprobs = logprobs.to(device='cuda')
 				loss = criterion(logprobs, dig_labels)
@@ -112,9 +123,7 @@ if __name__ == '__main__':
 					running_loss, running_accu = evaluation(
 						losses, accuracies, args.evalsize)	
 					gen_report(batch_number, running_loss, running_accu,
-						pred.item(), dig_labels.item())
-					writer.add_scalar('running_loss', running_loss, batch_number)
-					writer.add_scalar('accuracy', running_accu, batch_number)
+						pred.item(), dig_labels.item(), writer)
 			except KeyError:
 				print('Batch{}:index[{}:{}] missing gold label'
 					.format(batch_number, batch_i, batch_z))
